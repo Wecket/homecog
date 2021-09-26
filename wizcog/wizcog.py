ae18fda035ec91ff4eb345b2d33758da2da71d61
Wecket <flexnez@gmail.com>
2021-09-26 16:50:29 +0300

    Testing update

--- wizcog/wizcog.py
+++ wizcog/wizcog.py
@@ -13,7 +13,8 @@ class Wizcog(commands.Cog):
     async def lights(self, ctx):
         bulbs = await discovery.discover_lights(broadcast_space="192.168.1.255")
         
-        bulb = wizlight("bulbs[0].ip")
-        asyncio.run(await bulb.turn_on(PilotBuilder(rgb = (0, 128, 255))))
+        bulb = wizlight(bulbs[0].ip)
+        await bulb.turn_on(PilotBuilder(rgb = (0, 128, 255)))
         
         await ctx.send(f"Turning on: {bulbs[0].ip}")
+        
